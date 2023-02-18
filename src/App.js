1f952a5456c0cdea9adb89e9fde3e19fe56d5ad4
sycodes95 <sycodes95@gmail.com>
2023-02-17 17:56:05 -0800

    added light bulb, it moves to mouse position. added dev icons into 1 div for now.

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ import { CSSTransition } from 'react-transition-group';
 import { useEffect, useRef, useState } from 'react';
 import { ReactSVG } from 'react-svg';
 
+import JS_ICON from './icons/js.svg'
 
+import { ReactIcon } from 'devicon';
 
-import soundOn from './icons/volume-high.svg'
-import soundOff from './icons/volume-off.svg'
+import bulb from './images/bulb.png'
 
 import Icon from '@mdi/react';
 import { mdiRun, mdiFinance, mdiWeightLifter } from '@mdi/js';
@@ -19,68 +20,94 @@ import { mdiRun, mdiFinance, mdiWeightLifter } from '@mdi/js';
 
 function App() {
   const [muted, setMuted] = useState(true)
-  const [muteButtonIcon, setMuteButtonIcon] = useState(soundOff)
+  
   const [introText, setIntroText] = useState(null)
 
-  const videoRef = useRef(null)
-
-  const videoDes = ['video created with AI','видео создано с помощью ИИ', 'AI로 만든 영상', ]
-  
+  const bulbRef = useRef(null);
 
   const [index, setIndex] = useState(0)
   const introTextArray = [
 
   ]
 
-  useEffect(()=>{
-    const intervalId = setInterval(()=>{
-      setIndex(index => (index + 1) % videoDes.length)
-    }, 300)
-    return () => clearInterval(intervalId)
-  }, [])
+  const handleMouseMove = (event) => {
+    const x = event.clientX;
+    const y = event.clientY;
+    console.log(x);
+    bulbRef.current.style.marginLeft = `${x / 50}px`;
+    bulbRef.current.style.marginTop = `${y / 50}px`;
+  };
 
-  const handleMuteClick = () =>{
-    if(muted){
-      setMuted(false)
-      setMuteButtonIcon(soundOn)
-      videoRef.current.muted = !muted;
-    } else {
-      setMuted(true)
-      setMuteButtonIcon(soundOff)
-      videoRef.current.muted = true;
-    }
-  }
+  
+
+  useEffect(() => {
+    
+
+    document.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
   return (
     <div className='app-con '>
-
+      
+      
+        
+      
+      
       <div className='nav w-52 fixed left-2/4 -translate-x-1/2 h-8 bg-striped-content bg-opacity-80 text-white  
-      bottom-left-round bottom-right-round p-2 text-xs grid grid-cols-3 gap-x-4'>
+      bottom-left-round bottom-right-round p-2 text-xs grid grid-cols-3 gap-x-4 z-10'>
         <button className='menu transition-all'>ABOUT</button>
         <button className='menu transition-all'>PROJECTS</button>
         <button className='menu transition-all'>CONTACT</button>
       </div>
-      <div className='content-con max-h-screen grid  bg-dev ' >
+
+      
+      
+
+      <section className=' content-con max-h-screen grid  bg-dev corners' >
        
-        <div className='intro bg-dev-centered flex justify-center items-center h-screen w-full '>
-          <div className='border-opacity-0 font-shadow-white-light font-black-outline-light text-white flex flex-col justify-center  text-center'>
-            <span>Kevin <span className='text-xs'>승윤</span>  Kim</span>
-            <span>Fullstack Developer.</span>
+        <div className='bg-dev-centered-reversed flex justify-center items-center h-screen w-full '>
+
+          <div className='absolute h-full w-full max-w-full flex justify-center overflow-hidden'>
+          
+            <img className='absolute top-neg-20 pointer-events-none' src={bulb} ref={bulbRef}/>
+          </div>
+
+          <div className='border-opacity-0 flex flex-col justify-center text-center'>
+            <span className='font-shadow-white-light text-white'>Kevin <span className='text-xs'>승윤</span>  Kim</span>
+            <span className='font-shadow-white-light text-white '>Fullstack Developer.</span>
           </div>
         </div>
 
-      </div>
-      <div className='hidden'>
+      </section>
+      <div className='hidden '>
         <Icon path={mdiRun} size={1} />
         <Icon path={mdiFinance} size={1} />
         <Icon path={mdiWeightLifter} size={1} />
       </div>
 
-      <div className='h-screen w-full  bg-dev-bottom-dark border-t border-gray-700 grid'>
+      <div className='h-screen w-full  bg-dev-centered-reversed  border-gray-700 '>
         
-        <div className=' font-shadow-white text-center flex items-center justify-center row-span-1'>
-          <div className='h-36 text-center text-white text-6xl font-thin transition-all flex items-center'>
+        <div className=' text-center flex justify-center row-span-1'>
+          <div className='h-36 text-center text-white text-6xl font-thin transition-all flex items-center font-shadow-white '>
             ABOUT
           </div>
+          
+          
+        </div>
+
+        <div className='flex justify-center'>
+          <i class="devicon-react-original text-6xl text-white fill-current font-black-outline-light"></i>
+          <i class="devicon-javascript-plain text-6xl text-white fill-current font-black-outline-light"></i>
+          
+          <i class="devicon-html5-plain text-6xl text-white fill-current font-black-outline-light"></i>
+          <i class="devicon-css3-plain text-6xl text-white fill-current font-black-outline-light"></i>
+          <i class="devicon-tailwindcss-plain text-6xl text-white fill-current font-black-outline-light"></i>
+          <i class="devicon-nodejs-plain text-6xl text-white fill-current font-black-outline-light"></i>
+          <i class="devicon-express-original text-6xl text-white fill-current font-black-outline-light"></i>
+          <i class="devicon-mongodb-plain-wordmark  text-6xl text-white fill-current font-black-outline-light"></i>
         </div>
 
         
@@ -88,7 +115,7 @@ function App() {
       </div>
 
 
-      <div className='h-screen w-full bg-dev-top-dark border-t border-gray-700 grid'>
+      <div className='h-screen w-full bg-dev-centered-reversed  border-gray-700 grid'>
         
         <div className=' font-shadow-white text-center flex items-center justify-center row-span-1'>
           <div className='h-36 text-center text-white text-6xl font-thin transition-all flex items-center'>PROJECTS</div>
@@ -111,7 +138,7 @@ function App() {
 
       </div>
 
-      <div className='h-screen w-full bg-dev-bottom-dark border-t border-gray-700 grid'>
+      <div className='h-screen w-full bg-dev-centered-reversed border-gray-700 grid'>
         
         
 
