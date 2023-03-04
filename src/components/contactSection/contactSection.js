d51e60099e01a8470671f523b47e3738858acb43
sycodes95 <sycodes95@gmail.com>
2023-03-03 16:49:33 -0800

    added padding bottom on contact form

--- src/components/contactSection/contactSection.js
+++ src/components/contactSection/contactSection.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import Icon from '@mdi/react';
-import { mdiCrosshairs } from '@mdi/js';
+import { mdiCrosshairs, mdiEmail } from '@mdi/js';
 
 
 function ContactSection () {
@@ -20,9 +20,9 @@ function ContactSection () {
          bg-black bg-opacity-25 bg-striped
          h-full w-full 
          flex items-center 
-         border-2 border-black ">
+          ">
           <div className="flex justify-start ml-2 w-fit">
-            <Icon className=" text-red-700 text-opacity-50 bg-black bg-opacity-20 rounded-md" path={mdiCrosshairs} size={1.5} />
+            <Icon className=" text-red-700 text-opacity-50 bg-black bg-opacity-20 rounded-md" path={mdiEmail} size={1.5} />
           </div>
 
           <div className="flex justify-center ml-2 w-full font-bold font-black-outline-light text-red-700 text-opacity-70 text-xl">
@@ -30,7 +30,7 @@ function ContactSection () {
           </div>
 
           <div className="flex justify-end mr-2 w-fit">
-            <Icon className=" text-red-700 text-opacity-50 bg-black bg-opacity-20 rounded-md flip" path={mdiCrosshairs} size={1.5} />
+            <Icon className=" text-red-700 text-opacity-50 bg-black bg-opacity-20 rounded-md flip" path={mdiEmail} size={1.5} />
           </div>
           
           
@@ -38,7 +38,7 @@ function ContactSection () {
         
         
       </div>
-      <form className="CONTACT-FORM bg-black bg-opacity-25 flex flex-col gap-y-1 items-center p-2 text-white" target="_blank" action="https://formsubmit.co/123331be8f05163ba4dd9aeadde64f94" method="POST">
+      <form className="CONTACT-FORM bg-black bg-opacity-25 flex flex-col gap-y-1 items-center p-2 text-white pb-8 " target="_blank" action="https://formsubmit.co/123331be8f05163ba4dd9aeadde64f94" method="POST">
         
         <input className="hidden" type="text" name="_honey"/>
         <input type="hidden" name="_captcha" value="false"/>
